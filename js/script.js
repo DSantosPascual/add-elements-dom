da205f2bef0f593cd104c042b3b58bf442f5ffb5
Dámaso Adrián Santos Pascual <d.santos.pascual@gmail.com>
2024-11-07 17:21:53 +0100

    add-elements-dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
  // Aquí tu código
+
+const botonAgregar = document.getElementById("agregar");
+const lista = document.getElementById("lista");
+
+// Función //
+botonAgregar.addEventListener("click", function() {
+    // prompt //
+    const nuevoElemento = prompt("Introduce un nuevo elemento para la lista:");
+
+  //condicionall//  
+  if (nuevoElemento) {
+    const li = document.createElement("li");
+    li.textContent = nuevoElemento.trim(); // el trim elimina espacios al inicio y al final :)
+    lista.appendChild(li);
+} else {
+    alert("Por favor ingresa un elemento válido.");
+}
+});
